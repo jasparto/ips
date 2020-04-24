52a29fe50db07bbb076b91926244711b08629b94
juliano <juliano@INGSI08-PC.audi.d1>
2020-04-24 14:12:07 -0500

    Inicio Cupola

--- src/main/webapp/sistesa/cupola/service/Authorization.php
+++ src/main/webapp/sistesa/cupola/service/Authorization.php
@@ -4,9 +4,15 @@ include "../lib/config.php";
 require "../controller/ControlUser.php";
 
 $auth = $_SERVER['HTTP_AUTHORIZATION'];
+$dieMessage = "No puedes acceder a este recurso";
+
+if (!isset($auth)) {
+    die($dieMessage);
+}
+
 $security = explode(":", base64_decode($auth));
 $userController = new UserController();
 
-if ($userController->getCant($userController->validate($security[0], $security[1])) == 0) {
-    die("No puedes acceder a este recurso");
+if (isset($auth) || $userController->getCant($userController->validate($security[0], $security[1])) == 0) {
+    die($dieMessage);
 }
